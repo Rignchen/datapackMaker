c3d04ee27e222c05b5575a2f5b3fba7c38c34387
Rignchen <rinchendemort@gmail.com>
2024-01-03 21:42:22 +0100

    run the chosen template type

--- lib.py
+++ lib.py
@@ -1,7 +1,9 @@
 def choice(text:str, choices: list[str]):
-    for i in range(len(choices)):
-        print(f"{i+1}) {choices[i]}")
-    ans = ""
-    while not (len(ans) > 0 and ans.isdigit() and 0 < int(ans) <= len(choices)):
-        ans = input(text)
-    return int(ans) -1
+	if len(choices) == 0: raise IndexError("There needs to be at least 1 option to chose")
+	if len(choices) == 1: return 0
+	for i in range(len(choices)):
+		print(f"{i+1}) {choices[i]}")
+	ans = ""
+	while not (len(ans) > 0 and ans.isdigit() and 0 < int(ans) <= len(choices)):
+		ans = input(text)
+	return int(ans) -1
--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from os import listdir
+from importlib import import_module
 from lib import choice
 
-names = [i.removesuffix(".py") for i in listdir("script")]
+names = [i.removesuffix(".py") for i in listdir("template_type")]
+if "__pycache__" in names: names.remove("__pycache__")
 
-script = listdir("script")[choice("Wich template do you want? ",names)]
+templateType = names[choice("Wich template do you want? ",names)]
 
+script = import_module(f"template_type.{templateType}")
+script.start()
