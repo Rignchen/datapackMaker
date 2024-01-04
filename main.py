4d15b03dd9c27ee6267144ada8e999faac5560c5
Rignchen <rinchendemort@gmail.com>
2024-01-04 15:52:20 +0100

    adapt main to support ^C in template selection

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ names = [i.removesuffix(".py") for i in listdir(
 	path.join(getPath(__file__), "template_type"))]
 if "__pycache__" in names: names.remove("__pycache__")
 
-cls()
-templateType = names[choice("Wich template do you want? ",names)]
-cls()
-print(f"{templateType} template chosen\n")
-
 try:
-	import_module(f"template_type.{templateType}")
+	cls()
+	templateType = names[choice("Wich template do you want? ",names)]
+	cls()
+	print(f"{templateType} template chosen\n")
+
+	import_module(f"template_type.{templateType}") # run the selected script
 except KeyboardInterrupt:
 	print("\nCancel")
