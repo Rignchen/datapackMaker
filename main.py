263a6eab67f0ccdd748173b1554ca8d7c321041e
Rignchen <rinchendemort@gmail.com>
2024-01-24 14:40:46 +0100

    add ability to chose different template type Before you were only able to chose between a list of scripts Now you can chose a template type, then chose a script in the template type (or another template type)

--- main.py
+++ main.py
@@ -1,18 +1,42 @@
+from genericpath import isdir
 from importlib import import_module
 from lib.files import getPath
 from lib.i_o import choice, cls
-from os import listdir, path
+from os import listdir, path as osPath
 
-names = [i.removesuffix(".py") for i in listdir(
-	path.join(getPath(__file__), "template_type"))]
-if "__pycache__" in names: names.remove("__pycache__")
+def removeInList(array: list, values: list):
+	for value in values:
+		if value in array: array.remove(value)
+	return array
+
+defaultFolder = "template_type"
+
+cls()
+path = defaultFolder
 
 try:
-	cls()
-	templateType = names[choice("Wich template do you want? ",names)]
-	cls()
-	print(f"{templateType} template chosen\n")
+	while isdir(path):
+		templateName = path.replace(f'{defaultFolder}/','',1)
+		files = listdir(osPath.join(getPath(__file__), path))
+		removeInList(files,["scripts","__pycache__"])
+		files = [file.removesuffix(".py") for file in files]
 
-	import_module(f"template_type.{templateType}") # run the selected script
+		cls()
+		match len(files):
+			case 0:
+				print("No template found")
+				exit()
+			case 1:
+				chosenType = files[0]
+			case _:
+				if len(templateName) > 0:
+					print(f"Template type chosen: {templateName}\n")
+				chosenType = files[choice("Wich template do you want? ",files)]
+		path = osPath.join(path, chosenType)
+
+	templateName = path.replace(f'{defaultFolder}/','',1)
+	print(f"template chosen: {templateName}\n")
+
+	import_module(path.replace('/','.')) # run the selected script
 except KeyboardInterrupt:
 	print("\nCancel")
